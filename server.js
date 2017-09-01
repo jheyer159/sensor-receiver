33ccdb438464651c8bfe00303be9757daec49ded
Jeremy Heyer <jheyer159@gmail.com>
2017-08-31 18:44:15 -0700

    Changed emitter

--- server.js
+++ server.js
@@ -2,20 +2,18 @@ const express = require('express')
 const app = express()
 const bodyParser = require("body-parser")
 const dataEmitter = require("index.js")
-const jsonLint = require("jsonLint")
 
 var jsonParser = bodyParser.json({limit: '5mb'})
 
 app.post("/", jsonParser, function(req, res){
     try {
-        let json = jsonLint.parse(req.body)
-        dataEmitter.emit("data", json)
+        dataEmitter.emit('data', req.body)
         res.send("Data received successfully")
     }
     catch(e){
         dataEmitter.emit("error", e)
-        res.status(500).send("Invalid JSON") 
-    }  
+        res.status(500).send("Invalid request: " + e) 
+    } 
 })
 
 let port = process.env.SENSOR_PORT || "9191" 
