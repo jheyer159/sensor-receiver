427b0d6f0dc2f33374f7dd16400e98f0e478d175
Jeremy Heyer <jheyer159@gmail.com>
2017-08-31 18:44:02 -0700

    Removed jsonlint.

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 require("rootpath")()
 require("server.js")
 
-
 const EventEmitter = require('events')
 
 class Emitter extends EventEmitter {}
 
 const dataEmitter = new Emitter()
+
 dataEmitter.on('data', function(data){
     console.log("data: ", data)
 })
