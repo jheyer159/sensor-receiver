9c988b820deb6d61444a5aaf5bc3c0918b517882
Jeremy Heyer <jheyer159@gmail.com>
2017-09-02 13:08:09 -0700

    Added more tests

--- spec/listener.spec.js
+++ spec/listener.spec.js
@@ -1,4 +1,5 @@
 var Emitter = require('../index.js')
+var server = require('../server.js')
 
 describe('Data Emitter', function(){
     it('errors properly', function(){
@@ -6,5 +7,18 @@ describe('Data Emitter', function(){
         expect( function(){
             Emitter.emit('error', new Error("Whoops"))
         }).toThrow()
-    })  
+    }),
+
+    it('receives data', function(){
+        spyOn( Emitter, 'on')
+        expect( function(){
+            Emitter.emit('data', JSON.stringify({"test": "json"}))
+        }).toBe({"test": "json"})
+    })
+}),
+
+describe('Server', function(){
+    it('listens on a port', function(){
+
+    })
 })
