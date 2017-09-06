8cc873ae92d7aa81aa109a944f7bb32a8b1b1999
Jeremy Heyer <jheyer159@gmail.com>
2017-09-06 13:56:44 -0700

    Index only loads files.

--- index.js
+++ index.js
@@ -1,21 +1,2 @@
 require("./server.js")
 require("dotenv")
-
-const EventEmitter = require('events')
-
-class Emitter extends EventEmitter {}
-
-const dataEmitter = new Emitter()
-
-dataEmitter.on('data', function(data){
-    console.log("test: ", JSON.stringify(data))
-})
-dataEmitter.on('error', function(err){
-    let error = new Date() + ": dataEmitter - " + err
-    throw error
-})
-
-dataEmitter.emit('data', "JSON")
-
-
-module.exports = dataEmitter
