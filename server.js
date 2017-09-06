2d8ebfbf53c3cea8588a701dc11bd4ea28970482
Jeremy Heyer <jheyer159@gmail.com>
2017-09-06 13:57:13 -0700

    Cleaned up server routes for POST.

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 const express = require('express')
 const app = express()
 const bodyParser = require("body-parser")
-const dataEmitter = require("./index.js")
+const dataEmitter = require("./emitter")
 
 var jsonParser = bodyParser.json({limit: '5mb'})
 
 app.post("/", jsonParser, function(req, res){
-    try {
+    if( Object.keys(req.body).length != 0) {
         dataEmitter.emit('data', req.body)
-        res.send("Data received successfully")
+        res.status(200).send("Success")
     }
-    catch(e){
-        dataEmitter.emit("error", e)
-        res.status(500).send("Invalid request: " + e) 
-    } 
+    else {
+        dataEmitter.emit("error", "Invalid request")
+        res.status(500) 
+    }
 })
 
 let port = process.env.SENSOR_PORT || "9191" 
@@ -21,3 +21,5 @@ let port = process.env.SENSOR_PORT || "9191"
 app.listen( port, function(){
     console.log("Sensor server listening on: " + port)
 })
+
+module.exports = app
