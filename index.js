1b5cdc9dbcc72e1c8b8ce4c4a6f523f6f35ae47e
Jeremy Heyer <jheyer159@gmail.com>
2017-09-13 12:22:30 -0700

    Prepared files for running on gcloud

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-require("./server.js")
-require("dotenv")
+const dataEmitter = require("./emitter")
+
+function handlePOST (req, res){
+    if (req.get('content-type') != "application/json"){
+        dataEmitter.emit("error", "Content-type not JSON")
+        res.status(500)
+    }
+    else if( Object.keys(req.body).length != 0) {
+        dataEmitter.emit('data', req.body)
+        res.status(200).send("Success")
+    }
+    else {
+        dataEmitter.emit("error", "Invalid request")
+        res.status(500) 
+    }
+}
+
+exports.http = function http(req, res){
+    handlePOST(req, res)
+}
--- server.js
+++ server.js
@@ -1,12 +1,16 @@
-const express = require('express')
-const app = express()
-const bodyParser = require("body-parser")
+//const express = require('express')
+//const app = express()
+//const bodyParser = require("body-parser")
 const dataEmitter = require("./emitter")
 
-var jsonParser = bodyParser.json({limit: '5mb'})
+//var jsonParser = bodyParser.json({limit: '5mb'})
 
-app.post("/", jsonParser, function(req, res){
-    if( Object.keys(req.body).length != 0) {
+function handlePOST (req, res){
+    if (req.get('content-type') != "application/json"){
+        dataEmitter.emit("error", "Content-type not JSON")
+        res.status(500)
+    }
+    else if( Object.keys(req.body).length != 0) {
         dataEmitter.emit('data', req.body)
         res.status(200).send("Success")
     }
@@ -14,12 +18,8 @@ app.post("/", jsonParser, function(req, res){
         dataEmitter.emit("error", "Invalid request")
         res.status(500) 
     }
-})
+}
 
-let port = process.env.SENSOR_PORT || "9191" 
-
-app.listen( port, function(){
-    console.log("Sensor server listening on: " + port)
-})
-
-module.exports = app
+exports.http = function http(req, res){
+    handlePOST(req, res)
+}
